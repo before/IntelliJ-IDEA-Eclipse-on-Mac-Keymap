e5505385d63778f3a43a787ffc6ec4962326501e
gabu <tsukada.shouya@gmail.com>
2012-03-03 16:59:19 +0900

    Update preferences dir for IntelliJ IDEA 11 CE

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for I in ~/Library/Preferences/IntelliJIdea*; do
+for I in ~/Library/Preferences/IdeaIC11*; do
 	X="$I/keymaps/Eclipse on Mac.xml"
 	echo Installing as $X
-	curl "https://raw.github.com/thatha/IntelliJ-IDEA-Eclipse-on-Mac-Keymap/master/Eclipse%20on%20Mac.xml" > "$X"
+	curl "https://raw.github.com/gabu/IntelliJ-IDEA-Eclipse-on-Mac-Keymap/master/Eclipse%20on%20Mac.xml" > "$X"
 done
