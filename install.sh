44b87c28c8005407151e8c5a1081a060cb46fedb
André Rouél <andre.rouel@googlemail.com>
2012-12-08 23:20:17 +0100

    Changed path to IntelliJ IDEA users library to support version >= 11

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for I in ~/Library/Preferences/IntelliJIdea*; do
+for I in ~/Library/Preferences/Idea*; do
 	X="$I/keymaps/Eclipse on Mac.xml"
 	echo "Installing in $X"
 	cp -v "Eclipse on Mac.xml" "$X"
